8f5f6ad805d0e88d2d4cbd21c839006c379ff538
parkchangju12 <jusu0369@naver.com>
2021-06-04 15:35:38 +0900

    oracle_06_04

--- 예문/6장.sql
+++ 예문/6장.sql
@@ -48,3 +48,147 @@ AND     a.department_id = b.department_id(+);
 
 SELECT  a.employee_id, a.emp_name, b.department_id, b.department_name
 FROM    employees a,departments b;
+
+SELECT a.*
+FROM (SELECT a.sales_month, ROUND(AVG(a.amount_sold)) AS month_avg
+        FROM sales a,
+             customers b,
+             countries c
+        WHERE   a.sales_month BETWEEN '200001' AND '200012'
+        AND     a.cust_id = b.CUST_ID
+        AND     b.COUNTRY_ID = c.COUNTRY_ID
+        AND     c.COUNTRY_NAME = 'Italy'
+        GROUP BY a.sales_month
+        ) a,
+        (SELECT ROUND(AVG(a.amount_sold)) AS year_avg
+            FROM sales a,
+                customers b,
+                countries c
+        WHERE   a.sales_month BETWEEN '200001' AND '200012'
+        AND     a.cust_id = b.CUST_ID
+        AND     b.COUNTRY_ID = c.COUNTRY_ID
+        AND     c.COUNTRY_NAME = 'Italy'
+        ) b
+WHERE a.month_avg>b.year_avg;
+
+
+SELECT  SUBSTR(a.sales_month, 1, 4) as years,
+        a.employee_id,
+        SUM(a.amount_sold) AS amount_sold
+FROM    sales a,
+        customers b,
+        countries c
+WHERE   a.cust_id = b.CUST_ID
+AND     b.country_id = c.COUNTRY_ID
+AND     c.country_name = 'Italy'
+GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id;
+
+SELECT  years, MAX(amount_sold) AS max_sold
+FROM    (SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id,
+                SUM(a.amount_sold) AS amount_sold
+         FROM   sales a,
+                customers b,
+                countries c
+         WHERE  a.cust_id = b.CUST_ID
+         AND    b.country_id = c.COUNTRY_ID
+         AND    c.country_name = 'Italy'
+         GROUP  BY  SUBSTR(a.sales_month, 1,4), a.employee_id)K
+GROUP BY years
+ORDER BY years;
+
+
+SELECT  emp.years, emp.employee_id, emp.amount_sold
+FROM    (SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id,
+                SUM(a.amount_sold) AS amount_sold
+         FROM   sales a,
+                customers b,
+                countries c
+         WHERE  a.cust_id = b.CUST_ID
+         AND    b.country_id = c.COUNTRY_ID
+         AND    c.country_name = 'Italy'
+         GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id) emp, (SELECT years, MAX(amount_sold) AS max_sold
+         FROM   (SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id,
+                        SUM(a.amount_sold) AS amount_sold
+                 FROM   sales a,
+                        customers b,
+                        countries c
+                WHERE   a.cust_id = b.CUST_ID
+                AND     b.country_id = c.COUNTRY_ID
+                AND     c.country_name = 'Italy'
+                GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id
+                ) K
+        GROUP BY years
+    ) sale
+WHERE   emp.years = sale.years
+AND     emp.amount_sold = sale.max_sold
+ORDER by YEARS;
+
+
+
+
+
+SELECT  emp.years, emp.employee_id, emp.amount_sold, emp.amount_sold
+FROM    (SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id,
+                SUM(a.amount_sold) AS amount_sold
+         FROM   sales a,
+                customers b,
+                countries c
+         WHERE  a.cust_id = b.CUST_ID
+         AND    b.country_id = c.COUNTRY_ID
+         AND    c.country_name = 'Italy'
+         GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id) emp, 
+         (SELECT years, MAX(amount_sold) AS max_sold
+         FROM   (SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id,
+                        SUM(a.amount_sold) AS amount_sold
+                 FROM   sales a,
+                        customers b,
+                        countries c
+                WHERE   a.cust_id = b.CUST_ID
+                AND     b.country_id = c.COUNTRY_ID
+                AND     c.country_name = 'Italy'
+                GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id
+                ) K
+        GROUP BY years
+    ) sale, employees emp2
+WHERE   emp.years = sale.years
+AND     emp.amount_sold = sale.max_sold
+AND     emp.employee_id = emp2.employee_id
+ORDER by YEARS;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
